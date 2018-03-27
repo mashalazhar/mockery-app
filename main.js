2488561a6b25b3baccbcec8776fadbeaa8d27275
Mashal <azhar.mashal@gmail.com>
2018-03-27 10:35:05 -0400

    Updated carousel and modal

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 
 // MODAL
 
-  $(document).ready(function(){
-    $('.modal').modal();
+$(document).ready(function(){
+    $(function () {         
+        $('.modal').modal();
+        $('#modal1').modal('open');
+        $('.trigger-modal').modal(3000);
+    });
+})(jQuery); // will not open in 3 seconds?
+   
+// CAROUSEL
+
+ $('.carousel.carousel-slider').carousel({
+    fullWidth: true
   });
 
-
-// CAROUSELF
-
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-      
-      
